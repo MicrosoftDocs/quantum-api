e4b46ab411177c9a72643a5d800e50dd8d9e4edf
Bill Ticehurst <billti@hotmail.com>
2024-02-08 10:28:08 -0800

    Move warning to after first header

--- api/add-warning.mjs
+++ api/add-warning.mjs
@@ -6,18 +6,19 @@ const apiWarning = `
 > [!WARNING]
 > This documentation refers to the Classic QDK, which has been replaced by the Modern QDK.
 >
-> Please see <https://aka.ms/qdk.api> for the API documentation for the Modern QDK.
-`;
+> Please see <https://aka.ms/qdk.api> for the API documentation for the Modern QDK.  `;
 
 const magicWarning = `
 > [!WARNING]
 > This documentation refers to the Classic QDK, which has been replaced by the Modern QDK.
 >
-> The Modern QDK only supports the '%%qsharp' magic command.
-`;
+> The Modern QDK only supports the '%%qsharp' magic command.  `;
+
+const updateLimit = 100000;
 
 updateFiles("iqsharp-magic", magicWarning);
 updateFiles("qsharp", apiWarning);
+
 
 /**
  * @param {string} dirName
@@ -26,6 +27,8 @@ updateFiles("qsharp", apiWarning);
 function updateFiles(dirName, warningText) {
   // loop over all files in the "qsharp" directory
   const files = readdirSync(dirName);
+  let updatedFiles = 0;
+
   for (const file of files) {
     if (!file.endsWith(".md")) continue;
 
@@ -37,25 +40,32 @@ function updateFiles(dirName, warningText) {
     // Find the location of the second line consistenting of just "---"
     const lines = content.split("\n");
     let dashesFound = 0;
+    let headerFound = false;
     let insertIndex = 0;
     for (const line of lines) {
       ++insertIndex;
       if (line === "---") {
         dashesFound++;
-        if (dashesFound === 2) {
-          break;
+        if (dashesFound > 2) {
+          throw Error("Found more than two lines of dashes in the file before the main header");
         }
+      }
+      if (line.startsWith("# ") && dashesFound === 2) {
+        headerFound = true;
+        break;
       }
     }
 
     // Throw an error if not found
-    if (dashesFound !== 2) {
-      throw new Error("Could not find the second line of dashes in the file.");
+    if (!headerFound) {
+      throw new Error("Didn't find the main header in the file");
     }
 
     // Insert a warning message
     lines.splice(insertIndex, 0, warningText);
     // Write the file back to disk
     writeFileSync(fileName, lines.join("\n"));
+
+    if(++updatedFiles > updateLimit) return;
   }
 }
